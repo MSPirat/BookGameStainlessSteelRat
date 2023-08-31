929e904fb5fa020a83a4c2b0ae1e8a529f5d3b21
[Sergey Melnichenko] <[m1s3@mail.ru]>
2023-08-31 17:45:04 +0600

    Добавлена логика игры и игрового процесса (для теста)

--- app/src/main/java/com/github/mspirat/bookgamestainlesssteelrat/GameProcess.kt
+++ app/src/main/java/com/github/mspirat/bookgamestainlesssteelrat/GameProcess.kt
@@ -1,4 +1,80 @@
 package com.github.mspirat.bookgamestainlesssteelrat
 
-class GameProcess {
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class GameProcess : AppCompatActivity() {
+
+    private lateinit var mChapterText: TextView
+    private lateinit var mButtonContainer: LinearLayout
+    private lateinit var mScrollContainer: ScrollView
+    private var mChapterNumber: Int = 0
+    private lateinit var mSharedPref: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_process)
+
+        mButtonContainer = findViewById(R.id.button_container)
+        mScrollContainer = findViewById(R.id.scroll_container)
+        mChapterText = findViewById(R.id.chapter_text)
+
+        mSharedPref = getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE)
+
+        mChapterNumber = mSharedPref.getInt(getString(R.string.chapter_number), 0)
+        title = mSharedPref.getString(getString(R.string.chapter_title), "Начало")
+        Log.d("myLog", "Восстановили текущую главу: $mChapterNumber - $title")
+
+        gameProcess()
+
+        Log.d("myLog", "Запущен игровой активити")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveChapter()
+    }
+
+    private fun saveChapter() {
+        mSharedPref = getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = mSharedPref.edit()
+        editor.putInt(getString(R.string.chapter_number), mChapterNumber)
+        editor.putString(getString(R.string.chapter_title), title.toString())
+        editor.apply()
+        Log.d("myLog", "Сохранили текущую главу: $mChapterNumber")
+    }
+
+    private fun addButton(textButton: String, nextChapter: Int) {
+        val button: Button =
+            layoutInflater.inflate(R.layout.game_button, mButtonContainer, false) as Button
+        button.text = textButton
+        mButtonContainer.addView(button)
+        Log.d("myLog", "Создали кнопку: $textButton")
+
+        mScrollContainer.scrollTo(0, 0)
+
+        button.setOnClickListener {
+            mChapterNumber = nextChapter
+            mButtonContainer.removeAllViews()
+            gameProcess()
+        }
+    }
+
+    private fun gameProcess() {
+        when (mChapterNumber) {
+            0 -> {
+                title = getString(R.string.title_0)
+                mChapterText.setText(R.string.ch_0)
+                addButton(getString(R.string.way_0_30), 30)
+            }
+            // TODO Add more cases as needed
+        }
+    }
 }
--- app/src/main/java/com/github/mspirat/bookgamestainlesssteelrat/MainActivity.kt
+++ app/src/main/java/com/github/mspirat/bookgamestainlesssteelrat/MainActivity.kt
@@ -1,15 +1,69 @@
 package com.github.mspirat.bookgamestainlesssteelrat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mOpenGameButton: Button
+    private lateinit var mResetGameButton: Button
+    private lateinit var mSharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mOpenGameButton = findViewById(R.id.open_game_button)
+        mResetGameButton = findViewById(R.id.reset_game_button)
+
+        Log.d("myLog", "Запущен стартовый активити")
     }
 
-    fun openGame(view: View) {}
-    fun resetProgress(view: View) {}
+    override fun onResume() {
+        super.onResume()
+        updateUI()
+    }
+
+    private fun updateUI() {
+        mSharedPref = getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE)
+        val mChapterNumber = mSharedPref.getInt(getString(R.string.chapter_number), 0)
+
+        if (mChapterNumber > 0) {
+            mOpenGameButton.text = getString(R.string.continue_game)
+            mResetGameButton.visibility = View.VISIBLE
+        } else {
+            mOpenGameButton.text = getString(R.string.start_game)
+            mResetGameButton.visibility = View.INVISIBLE
+        }
+
+        Log.d("myLog", "Восстановили текущую главу и обновили кнопки")
+    }
+
+    fun openGame(view: View) {
+        val intent = Intent(this, GameProcess::class.java)
+        startActivity(intent)
+    }
+
+    fun resetProgress(view: View) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(R.string.reset_progress_title)
+            .setMessage(R.string.reset_progress_message)
+            .setNegativeButton(R.string.reset_progress_cancel, null)
+            .setPositiveButton(R.string.reset_progress_ok) { _, _ ->
+                mSharedPref =
+                    getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE)
+                val editor: SharedPreferences.Editor = mSharedPref.edit()
+                editor.putInt(getString(R.string.chapter_number), 0)
+                editor.apply()
+                updateUI()
+            }
+            .show()
+    }
 }
